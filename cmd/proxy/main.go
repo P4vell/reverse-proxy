5db4ff91442e8a3edef357f4bebda7a10563ecd6
Pawel Kownacki <kownacki_pawel@o2.pl>
2026-07-14 19:48:59 +0200

    rename

--- cmd/proxy/main.go
+++ cmd/proxy/main.go
@@ -51,13 +51,13 @@ func runHTTPServer(srv *http.Server) {
 }
 
 func shutdownServer(srv *http.Server) {
-	shutdownCtx, shutdownCancel := context.WithTimeout(
+	ctx, cancel := context.WithTimeout(
 		context.Background(),
 		10*time.Second,
 	)
-	defer shutdownCancel()
+	defer cancel()
 
-	if err := srv.Shutdown(shutdownCtx); err != nil {
+	if err := srv.Shutdown(ctx); err != nil {
 		log.Printf("HTTP server shutdown failed: %v", err)
 	}
 
